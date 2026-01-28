7b7405a19179d3b5207b7534ad42037bf35cc9c8
Aayushgit18 <aayushchoundiye@gmail.com>
2026-01-28 16:12:52 +0530

    Update outputs.tf

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,1 +1,8 @@
-output "name" { value = azurerm_kubernetes_cluster.this.name }
+output "kubeconfig" {
+  value     = azurerm_kubernetes_cluster.this.kube_config_raw
+  sensitive = true
+}
+
+output "oidc_issuer_url" {
+  value = azurerm_kubernetes_cluster.this.oidc_issuer_url
+}
