0c3cb6541646f148be57805305ba8a4d1dc6b77c
Ayush Choundiye <ayush.choundiye@nitorinfotech.com>
2026-01-28 15:38:09 +0530

    aks

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "<REPLACE_STORAGE_ACCOUNT>"
+    storage_account_name = "aksstatefile"
     container_name       = "tfstate"
     key                  = "dev/aks.tfstate"
   }
