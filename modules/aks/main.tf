03aa716ba9e4e1fa6caae4b1a4ff1713b75b0074
Aayushgit18 <aayushchoundiye@gmail.com>
2026-01-28 14:06:12 +0530

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -21,14 +21,13 @@ resource "azurerm_kubernetes_cluster" "this" {
   }
 
   default_node_pool {
-    zones               = ["1", "2", "3"]
-    name                = "system"
-    vm_size             = "Standard_DS2_v2"
-    enable_auto_scaling = true
-    min_count           = 2
-    max_count           = 3
-    vnet_subnet_id      = var.system_subnet_id
-  }
+  name                = "system"
+  vm_size             = "Standard_DS2_v2"
+  enable_auto_scaling = true
+  min_count           = 2
+  max_count           = 3
+  vnet_subnet_id      = var.system_subnet_id
+}
 
   oms_agent {
     log_analytics_workspace_id = var.log_analytics_id
